9d99a96773a9871e2fec73d32e05759e65ea9d63
Ahmed Kaddoura <akadd8@gmail.com>
2021-03-14 23:19:55 -0700

    Implement select image

--- parstagram/CameraViewController.swift
+++ parstagram/CameraViewController.swift
@@ -7,15 +7,32 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
-
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSubmit(_ sender: Any) {
+    }
+    
+    @IBAction func onTapCamera(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
